b7821a0de9871a8c7b81347ed9c59f9507759c61
S4nti4g077 <santiago.perez777@gmail.com>
2022-10-27 21:08:25 +0200

    added loop for random places

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,21 @@
 const mongoose = require("mongoose");
-const Campground = require("./models/campground");
+const Campground = require("../models/campground");
+const cities = require("./cities");
 
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/yelp-camp");
 }
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB();
