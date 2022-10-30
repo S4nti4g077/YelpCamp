648fa586706a8c046c1e468cc56628f7e9493a70
S4nti4g077 <santiago.perez777@gmail.com>
2022-10-30 16:57:13 +0100

    added link at initial page, added :id result

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+const campground = require("./models/campground");
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -17,15 +18,15 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({
-    title: "My Backyard",
-    description: "Cheap camping!",
-  });
-  await camp.save();
-  res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await campground.find({});
+  res.render("campgrounds/index", { campgrounds });
 });
 
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
 app.listen(3000, () => {
   console.log("serving on port 3000");
 });
