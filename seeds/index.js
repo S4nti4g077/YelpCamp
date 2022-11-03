7d873e13e9e629b7d7678df3fca934a0b1254a0f
S4nti4g077 <santiago.perez777@gmail.com>
2022-11-03 20:52:08 +0100

    added image to show.ejs and description

--- seeds/index.js
+++ seeds/index.js
@@ -14,9 +14,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/random/in-the-woods",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero, maiores quia commodi ipsum quasi similique aspernatur possimus fugiat odit dolores architecto, quod magni necessitatibus vel a laudantium iure ea esse?",
+      price,
     });
     await camp.save();
   }
